9e147a9ac8be0d75cf25d52114b48eaf730c00e8
root <root@lyf.lyf>
2026-02-02 19:19:47 +0800

    Fix: Restore missing images and clean up outdated data

--- docs/blog/blog.data.ts
+++ docs/blog/blog.data.ts
@@ -5,23 +5,4 @@ export interface BlogPost {
   url: string
 }
 
-export const data: BlogPost[] = [
-  {
-    "title": "Third post",
-    "description": "Lorem ipsum dolor sit amet",
-    "date": "Jul 22 2022",
-    "url": "/blog/third-post"
-  },
-  {
-    "title": "Second post",
-    "description": "Lorem ipsum dolor sit amet",
-    "date": "Jul 15 2022",
-    "url": "/blog/second-post"
-  },
-  {
-    "title": "First post",
-    "description": "Lorem ipsum dolor sit amet",
-    "date": "Jul 08 2022",
-    "url": "/blog/first-post"
-  }
-]
+export const data: BlogPost[] = []
